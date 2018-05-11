699086d3bf3b1a28fb6eb3b4e7afd817336dbe8f
abhishek.goyal2 <abhi25goel@gmail.com>
2018-05-11 17:31:05 +0530

    Commit login page

--- src/test/java/Academy/HomePage.java
+++ src/test/java/Academy/HomePage.java
@@ -5,11 +5,12 @@ import java.io.IOException;
 import org.testng.annotations.Test;
 
 import pageObjects.LandingPage;
+import pageObjects.LoginPage;
 
 public class HomePage extends BaseUd {
 
 	@Test
-	public void basePageNavigation() throws IOException
+	public void basePageNavigation() throws IOException, InterruptedException
 	{
 	  BaseUd base=new HomePage();
 	 driver= base.intializeDriver();
@@ -17,5 +18,14 @@ public class HomePage extends BaseUd {
 	 
 	LandingPage lp=new LandingPage(driver);
 	lp.getLogin().click();
+	Thread.sleep(300);
+	LoginPage log=new LoginPage(driver);
+	log.getemail().sendKeys("abhi25goel");
+	log.getpassword().sendKeys("function@01");
+	log.loginbutton().click();
+	Thread.sleep(300);
+	driver.close();
+	
+	
 	}
 }
